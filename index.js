b18fc75d40d8ad7b866da02e6839a6266f1d0891
scarter31191 <scarter31191@gmaile.com>
2021-03-22 22:51:21 -0400

    commit tree 980e86c888c67b89f093ec026513abc1c201e98d

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100 ) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max(2, 4, 6, 8, 10, 20);
 // Write your code here
